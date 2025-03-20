8fffdc043a59ab59e0a4a1b8e4bb5abb0c77efd5
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2025-03-20 12:23:34 +0530

    some changes

--- src/components/complete-form.tsx
+++ src/components/complete-form.tsx
@@ -7,7 +7,7 @@ export default function CompleteForm() {
         <main>
             <FormHeader />
             <div className="p-10 flex justify-center">
-                <div className="p-10 flex flex-col items-center bg-gradient-to-tr from-blue-950 to-yellow-950 text-white rounded-xl md:w-1/2 shadow-2xl h-[30vh]">
+                <div className="p-10 flex flex-col items-center bg-gradient-to-tr from-blue-950 to-yellow-950 text-white rounded-xl md:w-1/2 shadow-2xl">
                     <FormWrapper title="Submission recorded">
                         <div className="text-yellow-500">
                             Your submission has been successfully recorded, and we will review it shortly. If any further action is required, we will reach out to you.
